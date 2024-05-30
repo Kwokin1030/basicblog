9be73b3bf16bc1b370ec34cef47e117e43fff933
Kwokin1030 <148882268+Kwokin1030@users.noreply.github.com>
2024-05-30 16:22:21 -0700

    final project

---
+++ src/components/Fetch.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+
+const Fetch = () => {
+  const [blogs, setBlogs] = useState([]);
+  const [counts, setCounts] = useState(5);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setBlogs(data);
+      })
+  }, [counts]);
+
+  function clicked() {
+      setCounts(counts => counts + 5)
+  }
+
+  blogs.splice(counts, 100 - counts);
+
+  return (
+    <>
+      <h1>{counts}</h1>
+      <button className='btn pulse' onClick={clicked}>Show More</button>
+      <div class="content">
+        {
+          blogs.map((blog) => (
+            <p key={blog.id}><b>{blog.title}</b> <br /><br />{blog.body}<br /><br />USERID: {blog.userId}</p>
+          ))
+        }
+      </div>
+    </>
+  )
+}
+export default Fetch;
